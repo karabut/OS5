bde23ce4d560d949983f9f060266badde4c4971d
ekaterinakarabut <kes_n@mail.ru>
2021-05-05 14:40:42 +0300

    printing information about invalid input

--- main.cpp
+++ main.cpp
@@ -1,236 +1,249 @@
-#include <fcntl.h>
+#include <sys/types.h>
 #include <unistd.h>
+#include <fcntl.h>
 #include <stdlib.h>
 #include <stdio.h>
-#include <limits.h>
-#include <stdbool.h>
+#include <string.h>
+#include <errno.h>
 
-#define ERROR -1
-#define STOP_LINE 0
-#define TABLE_SIZE 256
-#define CONSOLE_INPUT_SIZE 30
-#define DECIMAL 0
-#define BUFFER_SIZE 1024
+extern int errno;
 
-long long readNum()
-{
-    long long numberOfTheLine;
-    char* pVoid = NULL;
-    char console_input[CONSOLE_INPUT_SIZE];
+#define ERROR_OPEN_FILE -1
+#define ERROR_CLOSE_FILE -1
+#define ERROR_READ -1
+#define ERROR_WRITE -1
+#define ERROR_ADD_TO_TABLE -1
+#define ERROR_GET_LINE_NUMBER -1
+#define ERROR_LSEEK -1
+#define ERROR_ADD_TO_TABLE -1
 
-    if(printf("Enter number of the line: ") == ERROR){
-        perror("Error in printing text for user");
-        return ERROR;
+#define SUCCESS_CLOSE_FILE 0
+#define SUCCESS_READ 0
+#define SUCCESS_WRITE 0
+#define SUCCESS_GET_LINE_NUMBER 1
+#define SUCCESS_ADD_TO_TABLE 0
+#define NO_ERROR 0
+
+#define STRING_EQUAL 0
+#define INVALID_LINE_NUMBER_INPUT 0
+#define READ_EOF 0
+#define TABLE_INIT_SIZE 100
+#define READ_INIT -1
+#define INPUT_SIZE 128
+#define TRUE 1
+#define STOP_INPUT 0
+#define DECIMAL_SYSTEM 10
+#define WITH_NEW_LINE 1
+#define WITHOUT_NEW_LINE 0
+
+typedef struct line_info {
+    off_t offset;
+    size_t length;
+} line_info;
+
+int add_to_table(line_info **table, long long *table_size, long long *table_length, off_t line_offset, size_t line_length) {
+    if (table == NULL || *table == NULL || table_size == NULL || table_length == NULL) {
+        fprintf(stderr, "Can't add element to table: Invalid argument(s)");
+        return ERROR_ADD_TO_TABLE;
     }
-    if (read(STDIN_FILENO, console_input, CONSOLE_INPUT_SIZE) == ERROR){
-        perror("Problems with reading your text");
-        return ERROR;
-    }
 
-    numberOfTheLine = strtoll(console_input, &pVoid, DECIMAL);
-
-    return numberOfTheLine;
-}
-
-
-int fillTable(int fileDescriptor, size_t* lineLengths, off_t* fileOffsets){
-
-    //буффер для считывания
-    char readBuffer[BUFFER_SIZE];
-
-    int numberOfReadElements =  BUFFER_SIZE;
-    int currentLineIndex = 1;
-    int fileOffsetIndex = 1;
-    int currentOffset = 0;
-
-    while (numberOfReadElements > 0){
-
-        //сколько считали из файла (максимально buffer size, если столько в файле есть)
-        numberOfReadElements = read(fileDescriptor, readBuffer, BUFFER_SIZE);
-
-        //проверка на ошибку read
-        if (numberOfReadElements == ERROR){
-            perror("Can't read current text");
-            return ERROR;
+    if (*table_length == *table_size) {
+        line_info *ptr = (line_info *)realloc(*table, 2 * (*table_size) * sizeof(line_info));
+        if (ptr == NULL) {
+            perror("Can't create table");
+            return ERROR_ADD_TO_TABLE;
         }
+        *table = ptr;
+        (*table_size) *= 2;
+    }
 
-        //цикл по количеству считанных из файла элементов
-        for (size_t i = 0; i < numberOfReadElements; i++){
+    line_info elem;
+    elem.offset = line_offset;
+    elem.length = line_length;
 
-            if(currentLineIndex > TABLE_SIZE){
-                perror("File is too long. More than 256 lines");
-                free(readBuffer);
-                return ERROR;
-            }
+    (*table)[*table_length] = elem;
+    (*table_length)++;
 
-            //добавляем к длине строки по +1
-            lineLengths[currentLineIndex]++;
+    return SUCCESS_ADD_TO_TABLE;
+}
 
-            //добавляем к сдвигу по +1
-            currentOffset++;
+line_info *create_table(int fildes, long long *table_length) {
+    if (table_length == NULL) {
+        fprintf(stderr, "Can't create table: Invalid argument\n");
+        return NULL;
+    }
 
-            //если элемент - переход на новую строку
-            if (readBuffer[i] == '\n'){
+    long long size = TABLE_INIT_SIZE;
+    size_t line_length = 0, read_length = 1;
+    off_t line_offset = 0;
+    ssize_t read_check = READ_INIT;
+    char c;
 
-                fileOffsets[fileOffsetIndex] = (off_t)(currentOffset - lineLengths[currentLineIndex]);
 
-                currentLineIndex++;
+    *table_length = 0;
+    line_info *table = (line_info *) malloc(size * sizeof(line_info));
+    if (table == NULL) {
+        perror("Can't create table");
+        return NULL;
+    }
 
-                fileOffsetIndex++;
+    line_offset = lseek(fildes, 0L, SEEK_SET);
+    if (line_offset == ERROR_LSEEK) {
+        perror("Can't get/set position in file");
+        free(table);
+        return NULL;
+    }
 
-            }
+    while (read_check != READ_EOF) {
+        read_check = read(fildes, &c, read_length);
+        if (read_check == ERROR_READ) {
+            perror("Can't read from file");
+            free(table);
+            return NULL;
         }
-    }
-
-    free(readBuffer);
-    //возвращаем количество строк
-    return (currentLineIndex);
-}
-
-int readLineFromFile(int fileDescriptor, int lineLength, off_t offset, char* line){
-
-    //устанавливаем смещение от начала файла
-    off_t lseekCheck = lseek(fileDescriptor, offset, SEEK_SET);
-    if (lseekCheck == ERROR){
-        perror("Seek error");
-        return ERROR;
-    }
-
-    //считываем нужную строку из файла
-    int readCheck = read(fileDescriptor, line, lineLength);
-
-    if (readCheck == ERROR){
-        perror("Can not read current text");
-        return ERROR;
-    }
-
-    return EXIT_SUCCESS;
-}
-
-int printLine(char* line, int lineLength){
-    int writeCheck = write(STDOUT_FILENO, line, lineLength);
-    if (writeCheck == ERROR){
-        perror("Can't print line");
-        return ERROR;
-    }
-
-    return EXIT_SUCCESS;
-}
-
-int getLines(int fileDescriptor, size_t* lineLengths, off_t* fileOffsets, int numberOfLines){
-    off_t offset = 0;
-    int lineLength = 0;
-    long long lineNumber = 0;
-
-    while(true){
-        //пользователь вводит нужную строку
-        lineNumber = readNum();
-
-        if (lineNumber == ERROR){
-            printf("%s", "Can't get number from user");
-            return ERROR;
-        }
-
-        //если введенное число слишком большое или наборот
-        if (lineNumber == LLONG_MAX || lineNumber == LLONG_MIN){
-            perror("Invalid line number");
+        if (read_check != READ_EOF && c != '\n') {
+            line_length++;
             continue;
         }
 
-        //если пользователь ввел 0
-        if (lineNumber == STOP_LINE)
-            break;
-
-        //если такой строки нет или пользователь ввел отрицательно значение
-        if (lineNumber > numberOfLines || lineNumber < 0){
-            printf("%s", "Invalid line number: No such line in file\n");
-            continue;
+        int add_check = add_to_table(&table, &size, table_length, line_offset, line_length);
+        if (add_check == ERROR_ADD_TO_TABLE) {
+            free(table);
+            return NULL;
         }
 
-        //если строка не нулевая
-        if (lineLengths[lineNumber] != 0){
-            offset = fileOffsets[lineNumber];
-            lineLength = lineLengths[lineNumber];
-            //создаем буфер под размер строки
-            char line[lineLength];
-
-
-            int readLineCheck = 0;
-            //считываем строку из файла в массив line
-            readLineCheck = readLineFromFile(fileDescriptor, lineLength, offset, line);
-
-            if (readLineCheck == ERROR){
-                printf("%s", "Can't read line");
-                free(line);
-                return ERROR;
-            }
-
-            int printLineCheck = 0;
-            //печатаем поолученную строку
-            printLineCheck = printLine(line, lineLength);
-            if (printLineCheck == ERROR){
-                printf("%s", "Can't print line");
-                free(line);
-                return ERROR;
-            }
-            free(line);
+        line_length = 0;
+        line_offset = lseek(fildes, 0L, SEEK_CUR);
+        if (line_offset == ERROR_LSEEK) {
+            perror("Can't get/set position in file");
+            free(table);
+            return NULL;
         }
-        printf("\n");
     }
-    return EXIT_SUCCESS;
+    return table;
 }
 
-int main(int argc, char* argv[]){
+int write_to_console(const char *buf, size_t length, int new_line) {
+    ssize_t write_check = write(STDOUT_FILENO, buf, length);
+    if (write_check == ERROR_WRITE) {
+        perror("Can't write to console");
+        return ERROR_WRITE;
+    }
+    if (new_line == WITH_NEW_LINE) {
+        return write_to_console("\n", 1, WITHOUT_NEW_LINE);
+    }
+    return SUCCESS_WRITE;
+}
 
-    //инициализируем файловый дискриптор...таблицу сдвигов и длин строк
-    int fileDescriptor = 0;
-    int linesNumber = 0;
-    off_t fileOffsets[TABLE_SIZE]  = {0};
-    size_t lineLengths[TABLE_SIZE]  = {0};
+int get_line_number(long long *line_num) {
+    char input[INPUT_SIZE + 1];
 
-    /*
-     * int open(const char *pathname, int flags); открывает файл pathname. Если файл не существует,
-     * то есть возможность (если O_CREAT указан как флаг) его создания
-     * Возвращаемое значение - файловый дискриптор (неотрицательный int)
-     * Если системный вызов выполнени успешно, то возвращается наименьший файловый дискриптор, еще не открытый процессом
-     * В случае ошибки возвращает -1
-     * */
-
-    fileDescriptor = open("text.txt", O_RDONLY); /* open for reading only */
-
-    //проверка открылся ли файл
-    if (fileDescriptor == ERROR){
-        perror("File can not be opened \n");
-        return EXIT_FAILURE;
+    int write_check = write_to_console("Enter line number: ", 19, WITHOUT_NEW_LINE);
+    if (write_check == ERROR_WRITE) {
+        return ERROR_GET_LINE_NUMBER;
     }
 
-    //получаем количество строк в файле и параллельно строим таблицы
-    linesNumber = fillTable(fileDescriptor, lineLengths, fileOffsets);
+    ssize_t bytes_read = read(STDIN_FILENO, input, INPUT_SIZE);
+    if (bytes_read == ERROR_READ) {
+        perror("Can't get line number");
+        return ERROR_GET_LINE_NUMBER;
+    }
+    if (bytes_read == 0) {
+        return INVALID_LINE_NUMBER_INPUT;
+    }
+    input[bytes_read] = '\0';
 
-    if (linesNumber == ERROR){
-        printf("Error with filling the table");
-        return EXIT_FAILURE;
+    char *endptr = input;
+    *line_num = strtoll(input, &endptr, DECIMAL_SYSTEM);
+
+    if (errno != NO_ERROR) {
+        perror("Can't convert given number");
+        errno = 0;
+        return INVALID_LINE_NUMBER_INPUT;
     }
 
-
-    int getLinesCheck = 0;
-    //от пользователя получаем нужную строку
-    getLinesCheck = getLines(fileDescriptor, lineLengths, fileOffsets, linesNumber);
-
-    if (getLinesCheck == ERROR){
-        printf("Error with printing lines");
-        return EXIT_FAILURE;
+    int compare1_result = strcmp(endptr, "\n");
+    int compare2_result = strcmp(endptr, "");
+    if (compare1_result != STRING_EQUAL && compare2_result != STRING_EQUAL) {
+        fprintf(stderr, "Number contains invalid symbols\n");
+        return INVALID_LINE_NUMBER_INPUT;
     }
 
-    int closeCheck = 0;
-    closeCheck = close(fileDescriptor);
+    return SUCCESS_GET_LINE_NUMBER;
+}
 
-    if (closeCheck == ERROR){
-        perror("Error with closing the file");
-        return EXIT_FAILURE;
+int read_line(int fildes, off_t offset, size_t length, char *buf) {
+    off_t lseek_check = lseek(fildes, offset, SEEK_SET);
+    if (lseek_check == ERROR_LSEEK) {
+        perror("Can't get/set position in file");
+        return ERROR_READ;
     }
 
-    free(fileOffsets);
-    free(lineLengths);
-    return EXIT_SUCCESS;
+    ssize_t read_check = read(fildes, buf, length);
+    if (read_check == ERROR_READ) {
+        perror("Can't read from file");
+        return ERROR_READ;
+    }
+
+    return SUCCESS_READ;
+}
+
+int main(int argc, char** argv) {
+    if (argc < 2) {
+        printf("Usage: %s <filename>\n", argv[0]);
+        return 0;
+    }
+
+    int fildes = open(argv[1], O_RDONLY);
+    if (fildes == ERROR_OPEN_FILE) {
+        perror("Can't open file");
+        return 0;
+    }
+
+    long long table_length = 0, line_num = 0;
+    line_info *table = create_table(fildes, &table_length);
+    if (table != NULL) {
+        while(TRUE) {
+            int get_line_num_check = get_line_number(&line_num);
+
+            if (get_line_num_check == ERROR_GET_LINE_NUMBER) {
+                break;
+            }
+
+            if (get_line_num_check == INVALID_LINE_NUMBER_INPUT) {
+                continue;
+            }
+
+            if (line_num < 0 || line_num > table_length) {
+                fprintf(stderr, "Invalid line number. It has to be in range [0, %lld]\n", table_length);
+                continue;
+            }
+
+            if (line_num == STOP_INPUT) {
+                break;
+            }
+
+            off_t line_offset = table[line_num - 1].offset;
+            size_t line_length = table[line_num - 1].length;
+            char line[line_length];
+
+            int read_check = read_line(fildes, line_offset, line_length, line);
+            if (read_check == ERROR_READ) {
+                break;
+            }
+
+            int write_check = write_to_console(line, line_length, WITH_NEW_LINE);
+            if (write_check == ERROR_WRITE) {
+                break;
+            }
+        }
+
+        free(table);
+    }
+
+    int close_check = close(fildes);
+    if (close_check == ERROR_CLOSE_FILE) {
+        perror("Can't close file");
+    }
+    return 0;
 }
